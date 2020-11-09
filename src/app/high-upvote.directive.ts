156f24394d2d8db69b12b92bf3f505867e513028
abdisamad100 <abdisamadcade004@gmail.com>
2020-11-09 11:33:37 +0300

    add directive

--- src/app/high-upvote.directive.ts
+++ src/app/high-upvote.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,Input } from '@angular/core';
+import { Quote} from './quote'
 
 @Directive({
   selector: '[appHighUpvote]'
 })
 export class HighUpvoteDirective {
+  quotes: Quote;
 
-  constructor() { }
-
+  
+constructor(private el: ElementRef) {
+  this.highlight(this.highlightColor || this.defaultColor || 'red');
+ }
+@Input() defaultColor: string;
+@Input('myHighlight') highlightColor: string;
+ 
+ 
+private highlight(color: string) {
+  this.el.nativeElement.style.backgroundColor = color;
 }
+
+  
+  
+}
+
